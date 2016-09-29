00ee1ba434b34fce9fb065bec4fc531431244036
Brian Eng <brian@brianeng.com>
2016-09-29 15:02:26 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// React
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Sample data
+var SampleData = require('./movie-data');           // SampleData.movies
+var LotsMoreMovies = require('./lots-more-movies'); // LotsMoreMovies.movies
+
+ReactDOM.render(<App />, document.getElementById('app'));
